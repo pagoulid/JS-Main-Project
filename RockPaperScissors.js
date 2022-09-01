2d3e8718c170767bffc229fc800b6035ec9fc056
pagoulid <pagoulid@gmail.com>
2022-09-01 14:20:34 +0300

    Implementing game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,13 +1,25 @@
+/*GLOBAL OBJECTS*/
+const rock ={"paper":0,"scissor":1,"rock":1}; 
+const paper ={"paper":1,"scissor":0,"rock":1};
+const scissor ={"paper":1,"scissor":1,"rock":0};
+const elements ={"paper": paper,"scissor":scissor,"rock":rock}
+/*GLOBAL OBJECTS*/ 
+
 const playerSelection = userPlay();
 const computerSelection = computerPlay();
 console.log(playerSelection);
 console.log(computerSelection);
+const gameResult = playRound(playerSelection,computerSelection);
+
+console.log(gameResult);
+
+
 /* Returns User/Cpu guess */
 function userPlay(){
     let userGuess = prompt('It/s Rock Paper Scissors time! Pick a guess:');
 
     userGuess =userGuess.toLowerCase();
-    let validInput = guess=>(guess=="rock"||guess=="paper"||guess=="scissors");
+    let validInput = guess=>(guess=="rock"||guess=="paper"||guess=="scissor");
 
     while(!(validInput(userGuess))){
         userGuess = prompt('Invalid Input.Please pick a valid guess.');
@@ -20,7 +32,7 @@ function userPlay(){
 function computerPlay(){
     console.log('Waiting for computer/s guess...')
     let condition =Math.round(Math.random()*2); /*Random Number between 0-2*/
-    console.log(condition);
+    
 
     switch(condition){
         case 0:
@@ -28,7 +40,30 @@ function computerPlay(){
         case 1:
             return "paper";
         case 2:
-            return "scissors";
+            return "scissor";
     }
 }
 /* Returns User/Cpu guess */
+/*Game*/
+function playRound(pSelection,cSelection){
+    let userElement = elements[pSelection];
+    let computerElement = elements[cSelection];
+
+    let userScore = userElement[cSelection];
+    let computerScore = computerElement[pSelection];
+
+    if(userScore!=computerScore){
+
+        if(userScore>computerScore){
+            return `${pSelection} beats ${cSelection}. You win!`;
+        }
+        else{
+            return `${cSelection} beats ${pSelection}. You lose!`;
+        }
+
+    }
+    return `The result is Draw because both players selected ${pSelection}`;
+
+
+}
+/*Game*/ 
